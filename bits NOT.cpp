3899fe318e741f84aa6cf4b3f7873658018b3d13
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-08-15 09:16:52 +0530

    Add files via upload

---
+++ bits NOT.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
+int main()
+{
+  unsigned int a=8;
+  int b=8;
+  unsigned char c=8;
+  cout<<~a<<endl;
+  cout<<~b<<endl;
+  cout<<~c<<endl;
+  return 0;
+
+}//4294967287
+//-9
+//-9
+
