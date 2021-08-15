14fe577136de84bd8486bab9a04e62382cf4ad7d
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-08-15 09:36:20 +0530

    Add files via upload

---
+++ condition statement update in c++17.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
+int main()
+{
+  //UPDATE ON C++17 WE CAN INTIALISE IN CODITION //STATEMENT
+int i;
+if(int i=6;i/4==1){
+    cout<<"update of c++17";
+}
+else{
+    cout<<"not updated";
+}
+return 0;
+}
