bd07fdd2743b576a293519c12131a3dd8aed968a
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-08-15 13:01:54 +0530

    Add files via upload

---
+++ jump statement-goto.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+ 
+// function to print numbers from 1 to 10
+void printNumbers()
+{
+    int n = 1;
+label:
+    cout << n << " ";
+    n++;
+    if (n <= 10)
+        goto label;
+}
+ 
+// Driver program to test above function
+int main()
+{
+    printNumbers();
+    return 0;
+}//1 2 3 4 5 6 7 8 9 10
+
