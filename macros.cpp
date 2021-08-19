ab96ba70b2f9d75d6fb891ae43002ebec579f5e7
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-08-19 11:16:21 +0530

    Add files via upload

---
+++ macros.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#define limit 5
+using namespace std;
+int main()
+{
+    int a;
+    for(a;a<limit;a++){
+        cout<<a<<endl;
+    }
+    return 0;
+}//0
+//1
+//2
+//3
+//4
+
