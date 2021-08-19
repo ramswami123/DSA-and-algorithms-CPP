b9bb1807a8242923bf2a31333bf3fae9cd1818c7
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-08-19 10:52:43 +0530

    Add files via upload

---
+++ inputbuffersolution.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<limits>
+#include<ios>
+using namespace std;
+int main()
+{
+  int a;
+  string b;
+  cin>>a;
+
+  cin.sync();
+  cin>>b;
+
+  cout<<a<<endl;
+  cout<<b<<endl;
+  
+  return 0;
+}//input:56
+//dgfgde
+//output:56
+//dgfgde
