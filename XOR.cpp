96bff9fceebd516b0284226ebb7cdc8d82ad4b3d
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-08-25 11:40:05 +0530

    Add files via upload

---
+++ XOR.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
+int main()
+{
+    //XOR REPRESENTED AS ^
+    int a=0^0;
+    cout<<a;
+    cout<<(1^1);
+    cout<<(0^1);
+    cout<<(1^0);
+    return 0;
+}
