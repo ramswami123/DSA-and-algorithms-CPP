22151143b27f9b55027a7dff5c839cbc7ab3d540
ramswami123 <75007299+ramswami123@users.noreply.github.com>
2021-11-05 12:31:09 +0530

    Create BITSET.cpp

---
+++ BITSET.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bitset>//to deal with bitset operators
+int main(){
+	//create biset and assigning to variable
+	bitset<10>b(4);
+	//bitset<no.of bits>variable(value/integer)
+	//now b is array of binary 4
+	//b=0000000100
+	cout<<b[7];
+	//outputs 1
+}
